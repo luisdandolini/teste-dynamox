3af82dd5bd683ffa31ef6f8c8cf2ae2148164623
Luís Fernando Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2024-05-09 22:37:08 -0300

    feat: created component graphic

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -3,6 +3,7 @@ export default {
     BACKGROUND_900: "#263252",
     BACKGROUND_600: "#0165DB",
     BACKGROUND_400: "#F4F7FC",
+    BACKGROUND_300: "#F8FAFC",
 
     WHITE: "#FFFFFF",
     BLACK: "#37383D",
